621b0faf9b3998f370554487e0b9ae9f4dd5b1fa
kovitomi <kovitomi.sote@gmail.com>
2021-05-01 19:18:22 +0200

    heroku app created

---
+++ heroku/app.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st 
+
+@st.cache
+def load_data():
+    data = pd.read_csv("https://opendata.ecdc.europa.eu/covid19/nationalcasedeath/csv")
+    data['week'] = data.year_week.apply(lambda x: convert(x))
+    return data
+
+def convert(x):
+    year, week = x.split("-")
+    year = (int(year) - 2020) * 54
+    return year + int(week)
+
+data = load_data()
+
+hun = data[data.country == 'Hungary']
+
+fig = px.line(data_frame = hun, x = 'week', y = 'cumulative_count', color = 'indicator')
+
+
+st.title("Hello World!")
+country = st.selectbox("Select a country",["Hungary", "Belgium"])
+st.write(f"The selected country is: {country}")
+st.plotly_chart(fig)
